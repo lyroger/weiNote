9d4a24194cb8f5f42f1b3277ecc3c8e483f7ef53
luoyan <yan1079@126.com>
2017-04-19 11:26:32 +0800

    初始化工程

---
+++ Pods/Target Support Files/Pods-weiNoteTests/Pods-weiNoteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weiNoteTests : NSObject
+@end
+@implementation PodsDummy_Pods_weiNoteTests
+@end
---
+++ weiNote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weiNote
+//
+//  Created by luoyan on 2017/4/19.
+//  Copyright © 2017年 roger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
